9a23a8ea57d7d9396f1b273218802a8838a06a2d
Sharma.V <94122920+sharmavel@users.noreply.github.com>
2021-11-22 20:25:15 +0530

    Update Customer_Billing.h

--- 3_implimentation/inc/Customer_Billing.h
+++ 3_implimentation/inc/Customer_Billing.h
@@ -1,1 +1,19 @@
+/** 
+* @file customer_billing.h
+* 
+*
+*/
+#ifndef_billing_H
+#define_billing_H
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+float amount(float rate, int quantity, float discount, float vat);
+int j, quantity, cash, w = 0;
+long cash_received, newt_amount;
+float rate, discount, vat, t_amount=0, amount1, float_part;
+char itemcode[5], date[20];
+
+#endif
