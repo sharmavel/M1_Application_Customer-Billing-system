ba8808b1e334b722328ebd25a301fdbac1750a15
Sharma.V <94122920+sharmavel@users.noreply.github.com>
2021-11-22 20:47:16 +0530

    Update Customer_Billing.c

--- 3_implimentation/src/Customer_Billing.c
+++ 3_implimentation/src/Customer_Billing.c
@@ -1,1 +1,66 @@
-
+#include<customer_billing.h>
+float amount(float rate, int quantity, float discount, float vat);
+void main()
+{
+	for(j = 1; j!= 0; j++)
+	{
+		fgets(itemcode, 5, stdin);
+		if(strcmp(itemcode, "1\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "2\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "3\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "4\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "5\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "6\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "7\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "8\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, "9\n") == 0)
+		{
+		}
+		else if(strcmp(itemcode, "10\n") == 0)
+		{
+			
+		}
+		else if(strcmp(itemcode, " \n") == 0)
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
+	
+	if(float_part > 0.50)
+		
+	if(cash_received < newt_amount)
+	{
+	}
+	
+}
+float amount(float p, int q, float r, float s)
+{
+}
